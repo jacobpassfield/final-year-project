c14539affd15965523ec26a63e2afafa917adcce
Jacob Passfield <jacob_passfield@yahoo.co.uk>
2021-04-05 20:35:29 +0100

    Adding files.

---
+++ australia-map.R
@@ -0,0 +1,65 @@
+# Loading necessary libraires
+library(tidyverse)
+library(ggmap)
+# Ensure to cite ggmap "citation("ggmap")"
+
+# Load data
+load(file = "data/data.RData")
+
+# Geogroup consists of several surveys with different latitude and longitude
+# coordinates. Find the average longitude and latitude cordinates for each geogroup.
+lonlat_data <- data %>%
+  group_by(Geogroup) %>%
+  summarise(Latitude = mean(SiteLat), Longitude = mean(SiteLong))
+
+# Find minimum and maximum longitude and latitude coordinates to aid the 
+# creation of a bounding box.
+which.max(lonlat_data$Latitude)
+lonlat_data[284,]$Latitude # -9.527617
+which.min(lonlat_data$Latitude)
+lonlat_data[2,]$Latitude # -43.61616
+which.max(lonlat_data$Longitude)
+lonlat_data[136,]$Longitude # 167.9488
+which.min(lonlat_data$Longitude)
+lonlat_data[266,]$Longitude # 105.6571
+
+# Creating a bounding box
+bbox <- c(103, -45, 170, -5)
+
+# Retrieve map of Australia from Stamen Maps
+map <- get_map(location = bbox, source = "stamen", maptype = "toner-lite")
+# Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.
+
+# Check the map
+(ausMap <- ggmap(map))
+
+# Match data frame with bounding box
+lonlatAus <- lonlat_data %>% 
+  filter(bbox[1] <= Longitude, Longitude <= bbox[3], bbox[2] <= Latitude, Latitude <= bbox[4])
+# Overlaying geogroups onto map
+ausMap +
+  geom_point(aes(x = Longitude, y = Latitude), shape = 8, size = 3, colour = "red", data = lonlatAus) +
+  theme(legend.position="none")
+
+# SAVE MAP
+
+# pdf to call the plot
+pdf(file = "MapOfAustralia.pdf")
+
+# Create the plot
+ausMap +
+  geom_point(aes(x = Longitude, y = Latitude), shape = 8, size = 3, colour = "red", data = lonlatAus) + 
+  theme(legend.position="none")
+
+# Create the file
+dev.off()
+
+
+
+
+
+
+
+
+
+
---
+++ filtered-data.R
@@ -0,0 +1,21 @@
+# Loading necessary libraries
+library(tidyverse)
+
+# Loading main data
+load(file = "data/fish_data.RData")
+
+selected_data <- main_data %>%
+  rename("TaxonomicName" = "TAXONOMIC_NAME", "SpeciesEpithet" = "SPECIES_EPITHET",
+         "Genus" = "GENUS", "Family" = "FAMILY", "Order" = "ORDER", "Day" = "day",
+         "Month" = "month", "Year" = "year", "Geogroup" = "geogroup", 
+         "SpeciesMedianSST" = "midpoint", "MeanSST" = "meansst") %>%
+  select(Geogroup, SiteLat, SiteLong, SiteCode, Location, Day, Month, Year, Diver,
+         SurveyID, TaxonomicName, SpeciesEpithet,Genus, Family, Order, SizeClass, 
+         MaxSizeObs, MeanSST, SpeciesMedianSST) %>%
+  arrange(Geogroup, SiteCode, Day, Month, Year)
+
+# Renaming selected data as data
+data <- selected_data
+
+# Saving data
+save(data, file = "data/data.RData")
